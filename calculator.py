24eff0efae07c61b96df29ce6c0cea1ebb68bb74
Carlos <carlos.mendoza007@hotmail.com>
2023-05-08 18:07:01 -0500

    adelantamo y ya pide los datos al usuario, ofrece el menú e intentamos que multiplicara pero arroja error

--- calculator.py
+++ calculator.py
@@ -3,6 +3,11 @@ aquí vamos a programar una calculadora sencilla, que realice operaciones sencil
 y principales como sumar, restar, multiplicar y dividir con valores
 que se soliciten al usuario y que el usuario ingrese
 '''
+
+# definiremos las variables a utilizar en cada función que serán globales
+save_num = 0
+values_to_op = []
+
 #creamos la función por la cual le pediremos al usuario los valores con los que realizaremos las operaciones
 
 def ask_num():
@@ -18,11 +23,17 @@ def ask_num():
         print('Por favor ingrese un número mayor que cero para poder continuar con el programa...')
       elif save_num > 0:
         print(f'Vas a operar {save_num} veces')
-        values_to_op = float(input('Por favor ingrese un número: '))
+        dato = float(input('Por favor ingrese un número: '))
+        values_to_op.append(float(dato))
+        
+
+        
+        
+      
       acumulator += 1
       #acumulator = save_num
       
-      
+        
    
         
       
@@ -42,25 +53,43 @@ def show_menu():
                               +'3 Sumar\n'
                               +'4 Restar\n'
                               + '5 Finalizar\n>>> Escriba el número correspondiente a la operación que desea realizar: '))
-    while option_choice:
-       if option_choice == 1:
-          print('Usted ha seleccionado multiplicar sus valores')
-       elif option_choice == 2:
-          print('Usted ha seleccionado dividir sus valores')
-       elif option_choice == 3:
-          print('Usted ha seleccionado sumar sus valores')
-       elif option_choice == 4:
-          print('Usted ha seleccionado restar sus valores')
-       elif option_choice == 5:
-          print('Gracias por participar hasta pronto....')
-       else:
-          print('Por favor escoja un número de la lista.')
-    option_choice += 1
+   
+    
+    if option_choice == 1 and option_choice :
+         print('Usted ha seleccionado multiplicar sus valores')
+
+    elif option_choice == 2:
+         print('Usted ha seleccionado dividir sus valores')
+    elif option_choice == 3:
+         print('Usted ha seleccionado sumar sus valores')
+    elif option_choice == 4:
+         print('Usted ha seleccionado restar sus valores')
+    elif option_choice == 5:
+         print('Gracias por participar hasta pronto....')
+    else:
+         print('Por favor escoja un número de la lista.')
+    
+#show_menu()
 
 # creamos la funcion para la multiplicación
 
-#def multiplicate():
-   #save_num *= save_num
+def multiplicate():
+   global values_to_op
+
+   for multiplicar in (values_to_op):
+       values_to_op *= multiplicar
+      
+#haremos una función que imprima todo mientras que el usuario haga todo de manera correcta como lo pide el programa
+
+def print_all():
+    print(f'{ask_num()} {show_menu()} {multiplicate()}')
+
+
+print_all()
+
+    
+    
+
 
 
 
